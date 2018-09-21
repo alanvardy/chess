f1363448dcd2386d1c2f20d29e2265ce3c7286a2
Alan Vardy <alanvardy@gmail.com>
2018-09-21 16:28:01 -0600

    add class layout

---
+++ spec/pieces_spec.rb
@@ -0,0 +1,30 @@
+require 'pieces'
+
+describe Piece do
+  pending 'todo'
+end
+
+describe King do
+  pending 'todo'
+end
+
+describe Queen do
+  pending 'todo'
+end
+
+describe Rook do
+  pending 'todo'
+end
+
+describe Bishop do
+  pending 'todo'
+end
+
+describe Knight do
+  pending 'todo'
+end
+
+describe Pawn do
+  pending 'todo'
+end
+
