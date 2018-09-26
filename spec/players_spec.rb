0935b69618f2945b4e44e97208f9e9c8911ebc8c
Alan Vardy <alanvardy@gmail.com>
2018-09-26 05:47:19 -0600

    finish players_spec

--- spec/players_spec.rb
+++ spec/players_spec.rb
@@ -1,1 +1,14 @@
 require 'players'
+
+describe Player do
+  describe 'when initialized' do
+    it 'has a name' do
+      player = Player.new('test', 'purple')
+      expect(player.name).to eq('test')
+    end
+    it 'has a color' do
+      player = Player.new('test', 'purple')
+      expect(player.color).to eq('purple')
+    end
+  end
+end
