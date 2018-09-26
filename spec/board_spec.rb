02a93c17763b5daa1b39eda326f3c87378ef8fcb
Alan Vardy <alanvardy@gmail.com>
2018-09-26 05:56:12 -0600

    sketch out tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,5 +1,62 @@
 require 'board'
 
 describe Board do
-  pending 'todo'
+  describe 'when initialized' do
+    pending 'todo'
+  end
+
+  describe '#add_pieces' do
+    pending 'todo'
+  end
+
+  describe '#display' do
+    pending 'todo'
+  end
+
+  describe '#horizontal_line' do
+    pending 'todo'
+  end
+
+  describe '#select_square' do
+    pending 'todo'
+  end
+
+  describe '#input_coordinates' do
+    pending 'todo'
+  end
+
+  describe '#convert' do
+    pending 'todo'
+  end
+
+  describe '#identify' do
+    pending 'todo'
+  end
+
+  describe '#start' do
+    pending 'todo'
+  end
+
+  describe '#create_players' do
+    pending 'todo'
+  end
+
+  describe '#game' do
+    pending 'todo'
+  end
+
+  describe '#change_player' do
+    pending 'todo'
+  end
+
+  describe '#move' do
+  end
+
+  describe '#valid_move?' do
+    pending 'todo'
+  end
+
+  describe '#clear_screen' do
+    pending 'todo'
+  end
 end
